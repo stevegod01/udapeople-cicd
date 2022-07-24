d161b717790ddac9e8bec4f18da689df3fe3df21
stevegod01 <ejiogusebastine@gmail.com>
2022-07-24 09:24:39 +0100

    Correcting run migration error

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,11 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_HOST=udapeople-db.ci47juclhzlz.us-west-2.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_DATABASE=postgres
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export HALLY=boss
+export check="{{ lookup('env', 'TYPEORM_HOST')}}"
